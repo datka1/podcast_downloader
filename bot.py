4db0dda50a201a93ca31228f7f742707fd5aef25
David <chestofwonder@gmail.com>
2022-11-18 16:26:32 +0400

    last update

--- bot.py
+++ bot.py
@@ -9,12 +9,11 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ChatActions
 
 
-API_TOKEN = ""
-
+API_TOKEN = "YOUR TOKEN HERE"
 FFMPEG_PATH = "/home/david/telegram_bot/ffmpeg"
 DOWNLOAD_PATH = "/home/david/telegram_bot/podcast/"
-LOG_PATH = "/home/david/telegram_bot/podcast/links_log.txt"
 BAD_KEYWORDS = ["spotify", "ivoox"]
+LOG_PATH = "/home/david/telegram_bot/log.txt"
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -27,19 +26,20 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=["start", "help"])
 async def send_welcome(message: types.Message):
     await message.reply(
-        "▶️ Вставляем линк \ 🎧 Бот скачивает видео и высылает как аудио с минимальным размером \ ▶️ Paste video link here \ 🎧 Bot will download video and extract audio with minimal size"
+        "▶️ Вставляем линк\n🎧 Бот скачивает видео и высылает как аудио с минимальным размером\n⚙️ Если вы хотите помочь поддерживать этого бота или просто сказать спасибо чашечкой кофе ☕,➡️ https://paypal.me/dmgeo\n\n▶️ Paste video link here\n🎧 Bot will download video and extract audio with minimal size\n⚙️ If you want to help to maintain this bot or just what to say thanks with a cup of coffee ☕,➡️ https://paypal.me/dmgeo"
     )
 
 
 @dp.message_handler()
 async def download(message: types.Message):
+
     audio_list = []
 
     # Маленькое Логирование. Время/Линк/UserID
     time_now = datetime.today().strftime("%D %H:%M:%S")
     pasted_links_log = open(LOG_PATH, "a")
     pasted_links_log.write(
-        f"[{time_now}] | Link : {message.text} | UserID : {message.from_user.id} \n"
+        f"[{time_now}] Link: {message.text}, UserID: {message.from_user.id} \n"
     )
     pasted_links_log.close()
 
@@ -51,9 +51,10 @@ async def download(message: types.Message):
             if badkeyword == badurl or len(message.text.split(".")) <= 1:
                 bad_keyword_flag = 1
                 break
+    
     if bad_keyword_flag == 1:
         await message.answer(
-            "❌ Линк не поддерживается или это не линк, скачивание невозможно \ ❌ Link is not supported or its not a link, download is not possible"
+            "❌ Линк не поддерживается или это не линк, скачивание невозможно \n ❌ Link is not supported or its not a link, download is not possible"
         )
     else:
         await message.answer("⬇️ Скачиваю \ ⬇️ Downloading")
@@ -77,21 +78,22 @@ async def download(message: types.Message):
         )
 
         # Получаем дынные из popen = subprocess.Popen , Забираем оттуда проценты скачки и предеаем боту.
-        messagetoedit = await bot.send_message(
+        download_eta = await bot.send_message(
             message.chat.id, "⏳ Готово \ ⏳ Done : 0%"
         )
         for line in popen.stdout:
             if "ETA" in line:
                 down_percent = line.split()[1].replace("%", "")
-                time.sleep(3)
+                time.sleep(5)
                 try:
                     await bot.edit_message_text(
                         chat_id=message.chat.id,
-                        message_id=messagetoedit.message_id,
+                        message_id=download_eta.message_id,
                         text=f"⏳ Готово \ ⏳ Done : {down_percent}%",
                     )
                 except:
                     pass
+                    
         # Проверяем что-бы размер файла не превышал 45 МБ. Лимит 50МБ
         for podcast in glob.glob(f"{DOWNLOAD_PATH}{message.from_user.id}/*.m4a"):
             file_size = round(os.stat(podcast).st_size / (1024 * 1024))
@@ -113,6 +115,7 @@ async def download(message: types.Message):
                 os.remove(podcast)
             else:
                 pass
+        
         await message.answer("⬆️ Высылаю аудиофаилы \ ⬆️ Sending Files")
 
         for podcast in glob.glob(f"{DOWNLOAD_PATH}{message.from_user.id}/*.m4a"):
